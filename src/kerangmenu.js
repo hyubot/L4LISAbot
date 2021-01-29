63ef6ffff40a147fd40fbc2270cb09171b2f4809
hyubot <78199085+hyubot@users.noreply.github.com>
2021-01-29 18:21:04 +0700

    Add files via upload

---
+++ src/kerangmenu.js
@@ -0,0 +1,15 @@
+const kerangmenu = (prefix) => { 
+	return `
+
+┌──❬ Kerang Menu ❭──
+├≻ *${prefix}apakah [optional]*
+├≻ *${prefix}rate [optional]*
+├≻ *${prefix}bisakah [optional]*
+├≻ *${prefix}kapankah [optional]*
+├≻ *${prefix}gantengcek*
+├≻ *${prefix}toxic*
+├≻ *${prefix}cantikcek*
+├≻ *${prefix}persengay*
+╰──────────`
+}
+exports.kerangmenu = kerangmenu

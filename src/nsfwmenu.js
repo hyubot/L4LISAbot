06dd5ab240a2814162707ce23ffc6fe0ebdba72f
hyubot <78199085+hyubot@users.noreply.github.com>
2021-01-29 18:19:00 +0700

    Add files via upload

---
+++ src/nsfwmenu.js
@@ -0,0 +1,25 @@
+const nsfwmenu = (prefix) => { 
+	return `
+	
+┌──❬ Nsfw Menu ❭──
+├≻ *${prefix}randomhentai*
+├≻ *${prefix}hentai*
+├≻ *${prefix}nsfwblowjob*
+├≻ *${prefix}nsfwtrap*
+├≻ *${prefix}nsfwneko*
+├≻ *${prefix}loli*
+├≻ *${prefix}nsfwloli*
+├≻ *${prefix}bokep*
+├≻ *${prefix}kodenuklir2*
+├≻ *${prefix}randomanime*
+├≻ *${prefix}cry*
+├≻ *${prefix}kiss*
+├≻ *${prefix}randomhug*
+├≻ *${prefix}nekonime*
+├≻ *${prefix}waifu*
+├≻ *${prefix}waifu2*
+├≻ *${prefix}kodenuklir*
+├≻ *${prefix}nekopoi*
+╰──────────`
+}
+exports.nsfwmenu = nsfwmenu
---
+++ src/ownermenu.js
@@ -0,0 +1,25 @@
+const ownermenu = (prefix) => { 
+	return `
+	
+┌──❬ Owner Menu ❭──
+├≻ *${prefix}block 62858xxxxx*
+├≻ *${prefix}unblock 62858xxxxx*
+├≻ *${prefix}promote @tagmember*
+├≻ *${prefix}demote @tagadmin*
+├≻ *${prefix}bc*
+├≻ *${prefix}leave*
+├≻ *${prefix}bc2*
+├≻ *${prefix}leave*
+├≻ *${prefix}clearall*
+├≻ *${prefix}clone*
+├≻ *${prefix}hidetag*
+├≻ *${prefix}hidetag2*
+├≻ *${prefix}setprefix*
+├≻ *${prefix}unban*
+├≻ *${prefix}ban*
+├≻ *${prefix}runtime*
+├≻ *${prefix}turnoff*
+├≻  *${prefix}getses*
+├≻──────────`
+}
+exports.ownermenu = ownermenu
